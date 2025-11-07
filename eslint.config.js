2f7e7bd475e5349c5fafd8100409a098b62e03ee
Lindemberg Teixeira <lindemberglta@icloud.com>
2025-11-07 20:10:07 -0300

    chore: add dist folders to eslint ignore config

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default defineConfig([
-    globalIgnores(['dist']),
+    globalIgnores(['dist', 'dist-electron/', 'dist-react/', 'build/', 'node_modules/']),
     {
         files: ['**/*.{ts,tsx}'],
         plugins: { prettier },
